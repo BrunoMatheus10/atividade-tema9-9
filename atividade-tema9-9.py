af39f43c75a813170153f9a626a5a520f695801a
BrunoMatheus10 <brunomatheus88300432@gmail.com>
2024-09-17 09:16:21 -0300

    Add files via upload

---
+++ atividade-tema9-9.py
@@ -0,0 +1,5 @@
+x = int(input("digite o valor de x:"))
+y = int(input("digite o valor de y:"))
+print("valores originais - x:",{x}, "y:",{y})
+x,y = y,x
+print("valores trocados - x:",{x}, "y:",{y})
